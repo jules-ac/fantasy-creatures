e9594ea3653a334bf2514d4d3dc93d5d8201b87d
jules-ac <jules.aconti@gmail.com>
2026-09-22 14:56:19 -0400

    ok

--- src/Creature.java
+++ src/Creature.java
@@ -1,18 +1,141 @@
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Creature {
 
     private String name = "";
     private int power = 0;
 
+    // constructor
     public Creature(String n, int p) {
         name = n;
         power = p;
     }
+
+    public String getName() {
+        return name;
+    }
+    public int getPower() {
+        return power;
+    }
+
+    public static void displayPowerLevels(Creature[] crArr) {
+        for (Creature cr : crArr) {
+            System.out.println(cr.getName() + " - " + cr.getPower());
+        }
+    }
+
+    public static int getStrongestPower(Creature[] crArr) {
+        int strongestPower = crArr[0].getPower();
+        for (int i=1;i<crArr.length;i++) {
+            if (crArr[i].getPower() > strongestPower) {
+                strongestPower = crArr[i].getPower();
+            }
+        } return strongestPower;
+    }
+    public static int getWeakestPower(Creature[] crArr) {
+        int weakestPower = crArr[0].getPower();
+        for (int i=1;i<crArr.length;i++) {
+            if (crArr[i].getPower() < weakestPower) {
+                weakestPower = crArr[i].getPower();
+            }
+        } return weakestPower;
+    }
+
+    public static String getStrongestCreature(Creature[] crArr) {
+        String strongestCreature = "";
+        int strongestPower = getStrongestPower(crArr);
+        for (Creature cr : crArr) {
+            if (cr.getPower() == strongestPower) {
+                strongestCreature = cr.getName();
+                break;
+            }
+        } return strongestCreature;
+    }
+    public static String getWeakestCreature(Creature[] crArr) {
+        String weakestCreature = "";
+        int weakestPower = getWeakestPower(crArr);
+        for (Creature cr : crArr) {
+            if (cr.getPower() == weakestPower) {
+                weakestCreature = cr.getName();
+                break;
+            }
+        } return weakestCreature;
+    }
+    public static void displayArrayListCreatures(ArrayList<Creature> arrl) {
+        for (int i=0;i<arrl.size();i++) {
+            System.out.println(arrl.get(i).getName());
+        }
+    }
+    public static void arrayListScan(ArrayList<Creature> arrl, Scanner scan) {
+        String userSearch = "";
+        System.out.print("\nSearch collection for a creature.\nEnter creature name (case insensitive): ");
+        userSearch = scan.nextLine();
+        userSearch = userSearch.substring(0,1).toUpperCase() + userSearch.substring(1).toLowerCase();
+        for (Creature cr : arrl) {
+            if (  cr.getName().equals(userSearch)  ) {
+                System.out.println(userSearch + " is in your collection!");
+                return;
+            }
+        }
+        System.out.println("Creature not found.");
+    }
+    public static void compareArrayLists(ArrayList<Creature> refArr, ArrayList<Creature> baseArr) {
+        for (int i=0;i<refArr.size();i++) {
+            for (int y=0;y<baseArr.size();y++) {
+                if ((refArr.get(i)).getName().equals((baseArr.get(y)).getName())) {
+                    System.out.println((refArr.get(i)).getName());
+                }
+            }
+        }
+    }
+    // main method
     public static void main(String[] args) {
-        Creature dragon = new Creature("Dragon",55);
-        Creature pegasus = new Creature("Pegasus", 90);
-        Creature phoenix = new Creature("Phoenix", 42);
-        Creature ogre = new Creature("Ogre", 31);
-        Creature unicorn = new Creature("Unicorn", 86);
-        Creature[] critters = {dragon, pegasus, phoenix, ogre, unicorn};
+
+        Creature dragon = new Creature("Dragon",45);
+        Creature goblin = new Creature("Goblin", 80);
+        Creature phoenix = new Creature("Phoenix", 32);
+        Creature golem = new Creature("Golem", 95);
+        Creature unicorn = new Creature("Unicorn", 60);
+
+        Creature[] creatureArr = {dragon, goblin, phoenix, golem, unicorn};
+
+        displayPowerLevels(creatureArr);
+        System.out.println("\nStrongest Creature: " + getStrongestCreature(creatureArr) + " (" + getStrongestPower(creatureArr) + ")");
+        System.out.println("Weakest Creature: " + getWeakestCreature(creatureArr) + " (" + getWeakestPower(creatureArr) + ")");
+
+        // part 4: ArrayList
+        ArrayList<Creature> collection = new ArrayList<>();
+        collection.add(dragon);
+        collection.add(goblin);
+        collection.add(phoenix);
+        System.out.println("\nCurrent Collection:");
+        displayArrayListCreatures(collection);
+
+        Creature kraken = new Creature("Kraken", 88);
+        Creature griffin = new Creature("Griffin", 59);
+        collection.add(kraken);
+        collection.add(griffin);
+
+        collection.remove(goblin);
+
+        System.out.println("\nUpdated Collection");
+        displayArrayListCreatures(collection);
+
+        System.out.println("" + "\nTotal Creatures: " + collection.size());
+
+        // part 5: creature search
+        Scanner scan = new Scanner(System.in);
+        arrayListScan(collection, scan);
+
+        // part 6: rare creature challenge
+        ArrayList<Creature> rareCreatures = new ArrayList<>();
+        rareCreatures.add(dragon);
+        rareCreatures.add(phoenix);
+        rareCreatures.add(kraken);
+
+        System.out.println("\nRare Creatures Found:");
+        compareArrayLists(rareCreatures, collection);
     }
 }
---
+++ src/FantasyCreatureManager.java
@@ -0,0 +1,85 @@
+public class FantasyCreatureManager {
+
+
+    public static void displayPowerLevels(int[] arr) {
+        for (int power : arr) {
+            System.out.println(power);
+        }
+    }
+    public static void displayCreaturePowers(String[] crs, int[] pws) {
+        for (int i=0;i<crs.length;i++) {
+            System.out.println(crs[i] + " - " + pws[i]);
+        }
+    }
+    public static int getStrongestPower(int[] arr) {
+        int strongest = arr[0];
+        for (int power : arr) {
+            if (power > strongest) {
+                strongest = power;
+            }
+        } return strongest;
+    }
+    public static int getWeakestPower(int[] arr) {
+        int weakest = arr[0];
+        for (int power : arr) {
+            if (power < weakest) {
+                weakest = power;
+            }
+        } return weakest;
+    }
+    public static int getTotal(int[] arr) {
+        int totalPower = 0;
+        for (int power : arr) {
+            totalPower += power;
+        } return totalPower;
+    }
+    public static int getAboveFiftyCount(int[] arr) {
+        int aboveFifty = 0;
+        for (int power : arr) {
+            if (power>50) { aboveFifty++; }
+        } return aboveFifty;
+    }
+    public static String getStrongestCreature(String[] crs, int[] pws) {
+        int strongestPower = getStrongestPower(pws);
+        String strongestCreature = "";
+        for (int i=0;i<pws.length;i++){
+            if (pws[i] == strongestPower) {
+                strongestCreature = crs[i];
+                break;
+            }
+        } return strongestCreature;
+    }
+    public static String getWeakestCreature(String[] crs, int[] pws) {
+        int weakestPower = getWeakestPower(pws);
+        String weakestCreature = "";
+        for (int i=0;i<pws.length;i++) {
+            if (pws[i] == weakestPower) {
+                weakestCreature = crs[i];
+                break;
+            } else {
+                continue;
+            }
+        } return weakestCreature;
+    }
+    public static void main(String[] args) {
+
+        // part 1: creature power levels
+        int[] powers = {55, 90, 42, 31, 86};
+        String[] creatures = {"Dragon", "Pegasus", "Phoenix", "Ogre", "Unicorn" };
+
+        System.out.println("Power Levels:");
+        displayPowerLevels(powers);
+
+        System.out.println("\nTotal Power: " + getTotal(powers));
+        System.out.println("Strongest Power: " + getStrongestPower(powers));
+        System.out.println("Weakest Power: " + getWeakestPower(powers));
+        System.out.println("Creatures Above 50 Power: " + getAboveFiftyCount(powers));
+
+        // part 2: creature names
+        System.out.print("\n");
+        displayCreaturePowers(creatures, powers);
+
+        System.out.println("\nStrongest Creature: " + getStrongestCreature(creatures, powers) + " (" + getStrongestPower(powers) + ")");
+        System.out.println("Weakest Creature: " + getWeakestCreature(creatures, powers) + " (" + getWeakestPower(powers) + ")");
+    }
+}
